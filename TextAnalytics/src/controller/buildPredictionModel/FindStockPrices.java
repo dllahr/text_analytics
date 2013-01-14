5577bed0e40012225319d129429b850ed9818a90
dllahr <dlahr@broadinstitute.org>
2013-01-14 00:49:26 -0500

    add ability to calculate find/calculate average stock price for a given day index

--- TextAnalytics/src/controller/buildPredictionModel/FindStockPrices.java
+++ TextAnalytics/src/controller/buildPredictionModel/FindStockPrices.java
@@ -3,6 +3,7 @@ package controller.buildPredictionModel;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 
 import org.hibernate.Query;
@@ -21,6 +22,10 @@ public class FindStockPrices {
 			+ dayIndexParam + ") and company=(:" + companyParam + ")order by dayIndex asc";	
 	
 	private final List<StockData> stockDataList;
+	
+	FindStockPrices(List<StockData> stockDataList) {
+		this.stockDataList = stockDataList;
+	}
 	
 	public FindStockPrices(int minDayIndex, Company company) {
 		Query query = SessionManager.createQuery(queryStr);
@@ -56,6 +61,62 @@ public class FindStockPrices {
 			}
 		}
 		
+		return result;
+	}
+	
+	/**
+	 * 
+	 * @param dayIndexList list of dayIndexes that the weighted average of closing price
+	 * @param weights must contain an odd number doubles to be used in creating a weighted average
+	 * @return
+	 */
+	public Map<Integer, Double> findWeightedAverage(List<Integer> dayIndexList, double[] weights) {
+		final Map<Integer, Double> result = new HashMap<>();
+		
+		final int numDaysEachSide = weights.length / 2;
+		
+		Iterator<Integer> dayIndexIter = dayIndexList.iterator();
+		
+		ListIterator<StockData> stockDataIter = stockDataList.listIterator();
+		
+		while (dayIndexIter.hasNext() && stockDataIter.hasNext()) {
+			final int targetDayIndex = dayIndexIter.next();
+			
+			StockData stockData = null;
+			while (stockDataIter.hasNext() && 
+					(stockData = stockDataIter.next()).getDayIndex() < targetDayIndex) { }
+			
+			if (stockData.getDayIndex() >= targetDayIndex) {
+				final int currentIndex = stockDataIter.previousIndex();
+				
+				final int startIndex = currentIndex - numDaysEachSide >= 0 ? currentIndex - numDaysEachSide : 0;
+				final int startWeightIndex = numDaysEachSide - (currentIndex - startIndex);
+				
+				final int endIndex = currentIndex + numDaysEachSide < stockDataList.size() 
+						? currentIndex + numDaysEachSide : stockDataList.size() - 1;
+
+				double sum = 0.0;
+				int weightIndex = startWeightIndex;
+				double weightsSum = 0.0;
+				ListIterator<StockData> aveIter = stockDataList.listIterator(startIndex);
+				for (int i = startIndex; i <= endIndex; i++) {
+					StockData aveStockData = aveIter.next();
+					sum += weights[weightIndex] * aveStockData.getAdjustedClose();
+					
+					weightsSum += weights[weightIndex];
+					
+					weightIndex++;
+				}
+				
+				final double ave = sum / weightsSum;
+				
+				result.put(targetDayIndex, ave);
+			} else {
+				System.err.println("FindStockPrices findWeightedAverageNearDayIndexes requested dayIndex " 
+						+ targetDayIndex + " is after last day of StockData " + stockData.getDayIndex());
+			}
+		}
+
 		return result;
 	}
 
--- TextAnalytics/test/controller/buildPredictionModel/FindStockPricesTest.java
+++ TextAnalytics/test/controller/buildPredictionModel/FindStockPricesTest.java
@@ -2,6 +2,7 @@ package controller.buildPredictionModel;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -17,11 +18,13 @@ import orm.StockData;
 
 public class FindStockPricesTest {
 
+	private static final double eps = 1e-5;
+	
 	private static final int dayIndex = 9004;
 	private static final int dayIndexResult = 9006;
 	
 	@Test
-	public void test() {
+	public void testFindNext() {
 		List<Company> companyList = Utilities.convertGenericList(SessionManager.createQuery("from Company where id=2").list());
 		assertTrue(companyList.size() > 0);
 		
@@ -36,4 +39,141 @@ public class FindStockPricesTest {
 		assertTrue(result.get(dayIndex).getDayIndex() == dayIndexResult);
 	}
 
+	@Test
+	public void testFindWeightedAverage() {
+		int[] days = {0,1,2,3,4};
+		final double[] prices = {2.0, 3.0, 5.0};
+		
+		final double[] weights = {0.33, 1.0, 0.7};
+		
+		double expected = calcWeightedAve(prices, weights);
+		
+		double[] allPrices = null;
+		double actual = Double.NaN;
+		//test by moving target day around without cutting off edges
+		for (int targetDay = 1; targetDay  <= 3; targetDay++) {
+			allPrices = buildAllPrices(days.length, targetDay, prices);
+			
+			actual = runWeightedTest(days, targetDay, allPrices, weights);
+			
+			System.out.println(expected + " " + actual);
+			
+			assertEquals(expected, actual, (eps)*expected);
+		}
+		
+		//calculate on left edge
+		//prices:   2.0, 3.0, 5.0, 0.0, 0.0
+		//weights:  1.0, 0.7, ---, ---, ---
+		allPrices = buildAllPrices(days.length, 1, prices);
+		actual = runWeightedTest(days, 0, allPrices, weights);
+		expected = ((prices[0]*weights[1]) + (prices[1]*weights[2])) / (weights[1] + weights[2]);
+		System.out.println(expected + " " + actual);
+		assertEquals(expected, actual, (eps)*expected);
+		
+		//calculate on right edge
+		//prices:   0.0, 0.0, 2.0, 3.0, 5.0
+		//weights:  ---, ---, ---, 0.33, 1.0
+		allPrices = buildAllPrices(days.length, 3, prices);
+		actual = runWeightedTest(days, 4, allPrices, weights);
+		expected = ((weights[0]*prices[1]) + (weights[1]*prices[2])) / (weights[0] + weights[1]);
+		System.out.println(expected + " " + actual);
+		assertEquals(expected, actual, (eps)*expected);
+		
+		
+		//test where target day does not exactly match recorded days
+		days = new int[]{0, 2, 4, 6, 8};
+		expected = calcWeightedAve(prices, weights);
+		
+		//test by moving target day around without cutting off edges
+		for (int targetDay = 1; targetDay  <= 5; targetDay += 2) {
+			allPrices = buildAllPrices(days.length, (targetDay+1)/2, prices);
+			
+			actual = runWeightedTest(days, targetDay, allPrices, weights);
+			
+			System.out.println(expected + " " + actual);
+			
+			assertEquals(expected, actual, (eps)*expected);
+		}
+		
+		//calculate on left edge
+		//prices:   2.0, 3.0, 5.0, 0.0, 0.0
+		//weights:  1.0, 0.7, ---, ---, ---
+		allPrices = buildAllPrices(days.length, 1, prices);
+		actual = runWeightedTest(days, -1, allPrices, weights);
+		expected = ((prices[0]*weights[1]) + (prices[1]*weights[2])) / (weights[1] + weights[2]);
+		System.out.println(expected + " " + actual);
+		assertEquals(expected, actual, (eps)*expected);
+		
+		//calculate on right edge
+		//prices:   0.0, 0.0, 2.0, 3.0, 5.0
+		//weights:  ---, ---, ---, 0.33, 1.0
+		allPrices = buildAllPrices(days.length, 3, prices);
+		actual = runWeightedTest(days, 7, allPrices, weights);
+		expected = ((weights[0]*prices[1]) + (weights[1]*prices[2])) / (weights[0] + weights[1]);
+		System.out.println(expected + " " + actual);
+		assertEquals(expected, actual, (eps)*expected);
+
+	}
+
+
+	private static double runWeightedTest(int[] days, int targetDay, double[] prices, double[] weights) {
+		assertEquals(days.length, prices.length);
+				
+		final List<StockData> stockDataList = buildStockDataList(days, prices);
+		assertEquals(days.length, stockDataList.size());
+		
+		final FindStockPrices findStockPrices = new FindStockPrices(stockDataList);
+		
+		final List<Integer> dayIndexList = new ArrayList<>(1);
+		dayIndexList.add(targetDay);
+		
+		Map<Integer, Double> resultMap = findStockPrices.findWeightedAverage(dayIndexList, weights);
+		return resultMap.get(targetDay);
+	}
+	
+	private static double[] buildAllPrices(int numDays, int targetDayIndex, double[] prices) {
+		final double[] allPrices = new double[numDays];
+		for (int i = 0; i < allPrices.length; i++) {
+			allPrices[i] = 0.0;
+		}
+		
+		int priceIndex = 0;
+		final int halfNumWeights = prices.length / 2;
+		for (int i = targetDayIndex - halfNumWeights; i <= targetDayIndex + halfNumWeights; i++) {
+			allPrices[i] = prices[priceIndex];
+			priceIndex++;
+		}
+		
+		
+		return allPrices;
+	}
+	
+	private static double calcWeightedAve(double[] prices, double[] weights)  {
+		assertEquals(prices.length, weights.length);
+		
+		double weightsSum = 0.0;
+		double sum = 0.0;
+		for (int i = 0; i < prices.length; i++) {
+			weightsSum += weights[i];
+			sum += prices[i]*weights[i];
+		}
+		
+		return sum / weightsSum;
+	}
+	
+	private static List<StockData> buildStockDataList(int[] days, double[] prices) {
+		assertEquals(days.length, prices.length);
+
+		List<StockData> result = new ArrayList<>(prices.length);
+		
+		for (int i = 0; i < days.length; i++) {
+			StockData stockData = new StockData();
+			stockData.setDayIndex(days[i]);
+			stockData.setAdjustedClose(prices[i]);
+			
+			result.add(stockData);
+		}
+		
+		return result;
+	}
 }
