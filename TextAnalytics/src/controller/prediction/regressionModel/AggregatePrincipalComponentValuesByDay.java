a901e008f5808a3acaf3f1d8da7972e4509671cf
dllahr <dlahr@broadinstitute.org>
2013-06-13 06:06:59 -0400

    add code to aggregate principal component values for each article into average value per day

--- TextAnalytics/src/controller/prediction/regressionModel/AggregatePrincipalComponentValuesByDay.java
+++ TextAnalytics/src/controller/prediction/regressionModel/AggregatePrincipalComponentValuesByDay.java
@@ -1,8 +1,14 @@
 package controller.prediction.regressionModel;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import math.linearAlgebra.DenseFixedVector;
+import math.linearAlgebra.Vector;
 
 import org.hibernate.Query;
 
@@ -26,9 +32,65 @@ class AggregatePrincipalComponentValuesByDay {
 		
 		Map<DayEigenvalue, List<Double>> map = organizePcValByDayIndex(retrieveArticlePcValues(scoringModelId, minDayIndex));
 		
-		//TODO:  from above map, create vectors of pc values for each day
+		return aggregateByDay(map);
+	}
+	
+	
+	List<DayPrincipalComponentValueVector> aggregateByDay(Map<DayEigenvalue, List<Double>> map) {
 		
-		return null;
+		class Pair {
+			final DayEigenvalue dayEigenvalue;
+			final double average;
+			public Pair(DayEigenvalue dayEigenvalue, double average) {
+				this.dayEigenvalue = dayEigenvalue;
+				this.average = average;
+			}
+		}
+		
+		//for determining the number of eigenvectors
+		Set<Eigenvalue> eigSet = new HashSet<>();
+		
+		//convert the provided map into a list of key-value pairs, calculating the average of the list in map-value
+		List<Pair> pairList = new ArrayList<>(map.size());
+		for (DayEigenvalue dayEigenvalue : map.keySet()) {
+			eigSet.add(dayEigenvalue.eig);
+			
+			List<Double> valueList = map.get(dayEigenvalue);
+			
+			double sum = 0.0;
+			for (Double value : valueList) {
+				sum += value;
+			}
+
+			pairList.add(new Pair(dayEigenvalue, sum/valueList.size()));
+		}
+		
+		final int numEig = eigSet.size();
+		eigSet = null;
+		
+		ValueOperator<Pair, Integer, Vector> vo = new ValueOperator<Pair, Integer, Vector>() {
+			@Override
+			public Integer getKey(Pair t) {
+				return t.dayEigenvalue.dayIndex;
+			}
+			@Override
+			public Vector buildNew() {
+				return new DenseFixedVector(numEig);
+			}
+			@Override
+			public void addValue(Pair t, Vector aggregation) {
+				aggregation.setEntry(t.dayEigenvalue.eig.getSortIndex(), t.average);
+			}
+		};
+		
+		Map<Integer, Vector> dayIndexVectorMap = mapBuilder.build(vo, pairList);
+		
+		List<DayPrincipalComponentValueVector> result = new ArrayList<>(dayIndexVectorMap.size());
+		for (Integer dayIndex : dayIndexVectorMap.keySet()) {
+			result.add(new DayPrincipalComponentValueVector(dayIndex, dayIndexVectorMap.get(dayIndex)));
+		}
+		
+		return result;
 	}
 	
 	
--- TextAnalytics/test/controller/prediction/regressionModel/AggregatePrincipalComponentValuesByDayTest.java
+++ TextAnalytics/test/controller/prediction/regressionModel/AggregatePrincipalComponentValuesByDayTest.java
@@ -2,6 +2,8 @@ package controller.prediction.regressionModel;
 
 import static org.junit.Assert.*;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -113,5 +115,56 @@ public class AggregatePrincipalComponentValuesByDayTest {
 		DayEigenvalue de22 = new DayEigenvalue(day2.getDayIndex(), e2);
 		assertTrue(map.keySet().contains(de22));
 		assertEquals(2, map.get(de22).size());
+	}
+	
+	@Test
+	public void testAggregateByDay() {
+		Article day1 = new Article();
+		day1.setDayIndex(1);
+		Article day2 = new Article();
+		day2.setDayIndex(2);
+		
+		Eigenvalue e1 = new Eigenvalue();
+		e1.setSortIndex(0);
+		Eigenvalue e2 = new Eigenvalue();
+		e2.setSortIndex(1);
+		
+		List<ArticlePcValue> list = new LinkedList<>();
+		list.add(new ArticlePcValue(day1, e1, 1.1));
+		list.add(new ArticlePcValue(day1, e1, 2.2));
+		list.add(new ArticlePcValue(day1, e2, 3.3));
+		list.add(new ArticlePcValue(day1, e2, 4.4));
+		list.add(new ArticlePcValue(day1, e2, 5.5));
+		
+		list.add(new ArticlePcValue(day2, e1, 6.6));
+		list.add(new ArticlePcValue(day2, e2, 7.7));
+		list.add(new ArticlePcValue(day2, e2, 8.8));
+		
+		AggregatePrincipalComponentValuesByDay aggregator = new AggregatePrincipalComponentValuesByDay();
+		
+		Map<DayEigenvalue, List<Double>> map = aggregator.organizePcValByDayIndex(list);
+		
+		List<DayPrincipalComponentValueVector> result = aggregator.aggregateByDay(map);
+		assertNotNull(result);
+		assertEquals(2, result.size());
+		
+		Collections.sort(result, new Comparator<DayPrincipalComponentValueVector>() {
+			@Override
+			public int compare(DayPrincipalComponentValueVector o1, DayPrincipalComponentValueVector o2) {
+				return o1.dayIndex - o2.dayIndex;
+			}
+		});
+		
+		DayPrincipalComponentValueVector dpcvv = result.get(0);
+		assertEquals((int)day1.getDayIndex(), dpcvv.dayIndex);
+		assertEquals(1, dpcvv.prinCompValueVect.getMaxIndex());
+		assertEquals((1.1+2.2)/2.0, dpcvv.prinCompValueVect.getEntry(0), eps);
+		assertEquals((3.3+4.4+5.5)/3.0, dpcvv.prinCompValueVect.getEntry(1), eps);
+		
+		dpcvv = result.get(1);
+		assertEquals((int)day2.getDayIndex(), dpcvv.dayIndex);
+		assertEquals(1, dpcvv.prinCompValueVect.getMaxIndex());
+		assertEquals(6.6, dpcvv.prinCompValueVect.getEntry(0), eps);
+		assertEquals((7.7+8.8)/2.0, dpcvv.prinCompValueVect.getEntry(1), eps);
 	}
 }
