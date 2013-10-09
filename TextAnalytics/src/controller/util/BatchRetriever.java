5ae96badbb670d2be3836f6a4324fb063e2f70a6
dllahr <dlahr@broadinstitute.org>
2013-10-08 23:47:05 -0400

    simplify code - single loop instead of loop + extra if statement check to get 'leftovers'

--- TextAnalytics/src/controller/util/BatchRetriever.java
+++ TextAnalytics/src/controller/util/BatchRetriever.java
@@ -17,18 +17,16 @@ public class BatchRetriever<T> {
 	public List retrieveAll(List<T> inputList) {
 		List result = new LinkedList<>();
 		
-		final int numBatch = inputList.size() / batchSize;
+		final int numBatch = 1 + inputList.size() / batchSize;
 		
 		for (int i = 0; i < numBatch; i++) {
 			final int startInd = i*batchSize;
-			final int endInd = (i+1)*batchSize;
+			int endInd = (i+1)*batchSize;
+			if (endInd > inputList.size()) {
+				endInd = inputList.size();
+			}
 			
 			result.addAll(genericRetriever.retrieve(inputList.subList(startInd, endInd)));
-		}
-		
-		final int startInd = numBatch*batchSize;
-		if (startInd < inputList.size()) {
-			result.addAll(genericRetriever.retrieve(inputList.subList(startInd, inputList.size())));
 		}
 		
 		return result;
