90d1edfdbb39a32fe0aa4250fb7fad26372bbd2a
dllahr <dlahr@broadinstitute.org>
2013-10-14 14:35:27 -0400

    modify batch retriever to fix bug where last batch size could sometimes be 0

--- TextAnalytics/src/controller/util/BatchRetriever.java
+++ TextAnalytics/src/controller/util/BatchRetriever.java
@@ -17,16 +17,21 @@ public class BatchRetriever<T> {
 	public List retrieveAll(List<T> inputList) {
 		List result = new LinkedList<>();
 		
-		final int numBatch = 1 + inputList.size() / batchSize;
+		final int extra = inputList.size() % batchSize;
+		
+		final int numBatch = (inputList.size() / batchSize) + (extra > 0 ? 1 : 0);
 		
 		for (int i = 0; i < numBatch; i++) {
 			final int startInd = i*batchSize;
+			
 			int endInd = (i+1)*batchSize;
 			if (endInd > inputList.size()) {
 				endInd = inputList.size();
 			}
 			
-			result.addAll(genericRetriever.retrieve(inputList.subList(startInd, endInd)));
+			List<T> subList = inputList.subList(startInd, endInd);
+			
+			result.addAll(genericRetriever.retrieve(subList));
 		}
 		
 		return result;
