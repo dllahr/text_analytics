83d6cd3497e9ee2497c3bcbc4c3e71e20cda5420
dllahr <dlahr@broadinstitute.org>
2013-10-22 07:27:57 -0400

    added parameters for CAT

--- TextAnalytics/src/main/MainArticlesToPredictions.java
+++ TextAnalytics/src/main/MainArticlesToPredictions.java
@@ -38,15 +38,22 @@ public class MainArticlesToPredictions {
 		List<String[]> result = new LinkedList<>();
 		
 		switch (scoringModelId) {
-		case 2:
+		case 2: //GE
+			//load article parameters
 			result.add(new String[] {"2", articleDir});
+			//generate principal component values parameters
 			result.add(new String[] {"2", "2"});
+			//generate predictions parameters
 			result.add(new String[] {"8", "10,11", "2013-09-01", "2"});
-		case 4:
+		case 4: //MDLZ
 			result.add(new String[] {"1", articleDir});
 			result.add(new String[] {"4", "1"});
 			result.add(new String[] {"7", "8,9", "2013-09-01", "1"});
-		case 7:
+		case 6:  //CAT
+			result.add(new String[] {"5", articleDir});
+			result.add(new String[] {"6", "5"});
+			result.add(new String[] {"71", "16,17", "2011-09-05", "5"});
+		case 7: //MCD
 			result.add(new String[] {"6", articleDir});
 			result.add(new String[] {"7", "6"});
 			result.add(new String[] {"48", "14,15", "2013-09-01", "6"});
