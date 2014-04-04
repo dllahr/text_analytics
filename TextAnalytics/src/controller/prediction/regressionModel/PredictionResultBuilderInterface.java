bd871355a908a1b24169b9690d95eae8ab086324
dllahr <dlahr@broadinstitute.org>
2014-04-04 07:02:12 -0400

    add new interface to abstract what type of prediction is being made (regression vs. volatility)

---
+++ TextAnalytics/src/controller/prediction/regressionModel/PredictionResultBuilderInterface.java
@@ -0,0 +1,9 @@
+package controller.prediction.regressionModel;
+
+import java.util.List;
+
+public interface PredictionResulsBuilderInterface {
+
+	public void build(List<Prediction> predictionList);
+
+}
